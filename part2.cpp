ea9a4cb97c4b9489d572b1f45a29aafdc38e1254
Siveen3 <127445863+Siveen3@users.noreply.github.com>
2024-03-18 00:47:54 +0200

    Update part2.cpp

--- part2.cpp
+++ part2.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <deque>
 using namespace std;
+
 
 const int BUF_SIZE = 4096;
 char buffer1[BUF_SIZE], buffer2[BUF_SIZE];
+char token[100];
+char* readFromFile(const string& program, char buffer[]) {
 
-char* readFromFile(const string& fileName, char buffer[]) {
 
-    ifstream inputFile(fileName, ios::binary);
+
+    ifstream inputFile(program, ios::binary);
 
     // Check if the opening was successful
     if (!inputFile.is_open()) {
@@ -20,13 +24,47 @@ char* readFromFile(const string& fileName, char buffer[]) {
 
     // Check if the read was successful
     if (!inputFile) {
-        cerr << "Error reading from file: " << fileName << endl;
+        cerr << "Error reading from file: " << program << endl;
         return nullptr;
     }
 
     inputFile.close();
 
-    char* ptr;
-    ptr = buffer;
-    return ptr;
+    char* forward;
+    forward = buffer;
+    return forward;
+
+}
+
+void fff(){
+    char *forward=buffer1, *start=buffer1;
+    start=forward=readFromFile("program.txt", buffer1);
+    deque<string> myDeque;
+    bool Flag = false;
+    while(!Flag){
+        if(*forward==' '){
+            int i =0;
+            while((forward-1)!=start){
+                token[i] = *start;
+                start++;
+                i++;
+            }
+            token[i] = '\0';
+            myDeque.push_back(token);//pushes string not char
+            //myDeque.push_back("^");
+        }
+
+        switch(*forward++){
+        case '^':
+            if(*forward== buffer1[BUF_SIZE-1])
+                forward=readFromFile("program.txt", buffer2);
+            else if (*forward== buffer2[BUF_SIZE-1])
+                forward=readFromFile("program.txt", buffer1);
+            else 
+                Flag=true;
+
+    }
+
+    }
+
 }
