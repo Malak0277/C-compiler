1bbbfd1715d2c968b0dbece3d8974cee29401f1d
Mariam Abdou <21p0214@eng.asu.edu.eg>
2024-03-29 20:01:04 +0200

    Test

--- test.cpp
+++ test.cpp
@@ -1,2 +1,9 @@
-123abc; 
-0x5248++
+int main () {
+    for x = i / 3 x+5
+    y = 0b111 && z = 15;
+    s == "testtest"
+    if (y >=++x)
+        z---=++y|5
+        st <<= 01 idx * s
+    return 0;
+}
